e2d157934f3484d0c86d1787fe18669463a849b4
Shayan Khan <neonclanflamez@gmail.com>
2024-10-04 22:00:37 -0800

    final commit

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ private:
     int blue;
 
 public:
-
+    
     Color() : red(0), green(0), blue(0){}
 
     Color(int r, int g, int b) : red(r), green(g), blue(b){}
@@ -48,6 +48,17 @@ public:
 };
 
 int main(){
+
+    Color color1;
+    Color color2(255,0,0);
+    Color color3(128);
+    Color color4(0, 255, 255);
+
+    cout << "Color Values: " << endl;
+    color1.print();
+    color2.print();
+    color3.print();
+    color4.print();
 
     return 0;
 }
